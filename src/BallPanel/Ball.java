8c6a3e4c5527b03757114db7d2f4dcf7518262d3
ewq897456321 <ltesr125124015@gmail.com>
2022-12-06 19:23:15 +0800

    Transplant BallPlatePanel to GameKernel

--- src/BallPanel/Ball.java
+++ src/BallPanel/Ball.java
@@ -1,40 +1,24 @@
 package BallPanel;
 
+import GameKernel.utils.controllers.ImageController;
 import GameObject.Attribute;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.Objects;
 
-public class Ball extends JPanel {
+public class Ball {
     Attribute attribute;
-    private BufferedImage image;
+    public Image image;
     boolean isStrong = false;
-    int x;
-    int y;
+    public int x;
+    public int y;
 
     public Ball(int x, int y, Attribute attribute) {
         this.x = x;
         this.y = y;
         this.attribute = attribute;
-        try {
-            if (!isStrong)
-                image = ImageIO.read(Objects.requireNonNull(getClass().getResource("../Image/Balls/" + attribute + ".png")));
-            else // When isStrong ball get other than default img
-                image = ImageIO.read(Objects.requireNonNull(getClass().getResource(attribute + ".png")));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        this.setPreferredSize(new Dimension(80, 80));
-        this.setLayout(null);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        g.drawImage(image, 0, 0, 80, 80, null);
+        if (!isStrong)
+            image = (ImageController.instance().tryGetImage("../../../Image/Balls/" + attribute + ".png"));
+        else // When isStrong ball get other than default img
+            image = (ImageController.instance().tryGetImage("../Image/Balls/" + attribute + ".png"));
     }
 }
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,15 +1,87 @@
+import BallPanel.Ball;
+import GameKernel.utils.core.CommandSolver;
+import GameKernel.utils.core.GameKernel;
+import GameObject.Attribute;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GameFrame extends JFrame {
+    ArrayList<Ball> balls;
+    Ball ControlBall;
+    Random random;
+
     public GameFrame(String title, int width, int height) throws HeadlessException {
         super(title);
+        random = new Random();
+        balls = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                balls.add(new Ball(j * 100, i * 100, Attribute.values()[random.nextInt(6)]));
+            }
+        }
+
         this.setPreferredSize(new Dimension(width, height));
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel gamePanel = new GamePanel(1,1,1);
-        this.add(gamePanel);
+        GameKernel.GameInterface gi = new GameKernel.GameInterface() {
+            @Override
+            public void paint(Graphics g) {
+                for (Ball ball : balls) {
+                    g.drawImage(ball.image, ball.x, ball.y, 100, 100, null);
+                }
+            }
 
+            @Override
+            public void update() {
+
+            }
+        };
+        GameKernel gk = new GameKernel.Builder(gi, 60, 60)
+                .initListener()
+                .enableMouseTrack((e, state, trigTime) -> {
+                    if (state == CommandSolver.MouseState.DRAGGED) {
+                        Ball HitBall = checkMouseOnBall(e.getPoint());
+                        if (HitBall != null) {
+                            if (ControlBall == null) {
+                                ControlBall = HitBall;
+                            } else if (HitBall != ControlBall) {
+                                int tmpx = HitBall.x;
+                                int tmpy = HitBall.y;
+                                int indexControlBall = ControlBall.y / 100 * 5 + ControlBall.x / 100;
+                                int indexHotBall = HitBall.y / 100 * 5 + HitBall.x / 100;
+                                HitBall.x = ControlBall.x;
+                                HitBall.y = ControlBall.y;
+                                ControlBall.x = tmpx;
+                                ControlBall.y = tmpy;
+
+                                balls.set(indexControlBall, HitBall);
+                                balls.set(indexHotBall, ControlBall);
+                                ControlBall = null;
+                            }
+                        }
+                    }
+                    if (state == CommandSolver.MouseState.RELEASED) {
+                        ControlBall = null;
+                    }
+                })
+                .mouseForceRelease()
+                .gen();
+
+        this.add(gk);
         this.pack();
         this.setVisible(true);
+        gk.run(true);
+    }
+
+    public Ball checkMouseOnBall(Point e) {
+        int x = e.x;
+        int y = e.y;
+        for (Ball ball : balls) {
+            if (x >= ball.x && x < ball.x + 100 && y >= ball.y && y < ball.y + 100) return ball;
+        }
+        return null;
     }
 }
