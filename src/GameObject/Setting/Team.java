7de171cce0c3223a9d67cabf8096635cbc226605
張銘傑 <joonh3023203@gmail.com>
2023-01-06 00:37:56 +0800

    新增隊員

--- src/GameObject/Setting/Team.java
+++ src/GameObject/Setting/Team.java
@@ -17,18 +17,18 @@ public class Team {
 
     public Team() {
         team = new ArrayList<>();
-        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/Balls/None.png"),
+        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/player/秦皇.png"),
                 Attribute.DARK, 1200, 100, 500, 5, 150, 210));
-        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/Balls/None.png"),
+        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/player/項羽.png"),
                 Attribute.DARK, 1000, 100, 500, 5, 250, 210));
-        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/Balls/None.png"),
+        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/player/約娥.png"),
                 Attribute.WATER, 900, 100, 500, 5, 350, 210));
-        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/Balls/None.png"),
+        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/player/蚩尤.png"),
                 Attribute.FIRE, 3200, 100, 500, 5, 450, 210));
-        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/Balls/None.png"),
-                Attribute.TREE, 1250, 100, 500, 5, 550, 210));
-        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/Balls/None.png"),
-                Attribute.LIGHT, 900, 500, 1250, 5, 650, 210));
+        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/player/初號機.png"),
+                Attribute.TREE, 3250, 100, 500, 5, 550, 210));
+        team.add(new MainCharacter(ImageController.instance().tryGetImage("../../../Image/player/安妮亞.png"),
+                Attribute.LIGHT, 2000, 500, 1250, 5, 650, 210));
 
         calculateLife();
         teamLife = new LifeBar(-1, -1, (BALL_WIDTH * BALLPLATE_WIDTH), 20, life);
